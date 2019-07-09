f7fcb12500a052335a23b75c903370db5ebcc071
a-kataoka <a-kataoka@cookbiz.jp>
2019-07-09 17:38:27 +0900

    useEffect 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = ({
   props = {
@@ -8,6 +8,18 @@ const App = ({
 }) => {
   const [state, setState] = useState(props);
   const { name, price } = state;
+
+  useEffect(() => {
+    console.log("This is like componentDidMount or componentDidUpdate.");
+  });
+
+  useEffect(() => {
+    console.log("This is like componentDidMount.");
+  }, []);
+
+  useEffect(() => {
+    console.log("This callback is for name only.");
+  }, [name]);
 
   return (
     <>
