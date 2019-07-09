636e5eb40cc85d2898c77dbbf80f895988283474
a-kataoka <a-kataoka@cookbiz.jp>
2019-07-09 16:54:30 +0900

    useState 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { FC, useState } from "react";
 
-const App: React.FC = () => {
-  return <div>This is a template for React app.</div>;
+const App = ({
+  props = {
+    name: "",
+    price: 1000
+  }
+}) => {
+  const [state, setState] = useState(props);
+  const { name, price } = state;
+
+  return (
+    <>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>
+        -1
+      </button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input
+        type="text"
+        value={name}
+        onChange={e => setState({ ...state, name: e.target.value })}
+      />
+    </>
+  );
 };
 
 export default App;
