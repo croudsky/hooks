640ea4d0ab287341861f7d014e8dd0af65712c87
a-kataoka <a-kataoka@cookbiz.jp>
2019-07-17 10:59:42 +0900

    refactor constants

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,3 +1,5 @@
+import { CREATE_EVENT, DELETE_EVENT, DELETE_ALL_EVENTS } from "../actions";
+
 export interface IState {
   id: number;
   title: string;
@@ -9,14 +11,14 @@ const events = (
   action: { type: string; title: string; body: string; id: number }
 ) => {
   switch (action.type) {
-    case "CREATE_EVENT":
+    case CREATE_EVENT:
       const event = { title: action.title, body: action.body };
       const length = state.length;
       let id = length === 0 ? 1 : state[length - 1].id + 1;
       return [...state, { id, ...event }];
-    case "DELETE_EVENT":
+    case DELETE_EVENT:
       return state.filter(event => event.id !== action.id);
-    case "DELETE_ALL_EVENTS":
+    case DELETE_ALL_EVENTS:
       return [];
     default:
       return state;
